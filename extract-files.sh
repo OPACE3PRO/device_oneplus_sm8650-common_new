f757b1f74f249bf3984416b34f84500288cda218
John Galt <johngaltfirstrun@gmail.com>
2024-10-18 08:06:06 -0400

    extract-files: patch fingerprint service for task profiles
    Note:                                                                                                                                           We use top-app uclamp boosting on input, it is best to set this to
        top-app rather than foreground (HighPerformance)

--- extract-files.sh
+++ extract-files.sh
@@ -74,6 +74,9 @@ function blob_fixup() {
             sed -i "s/IFingerprint\/default/IFingerprint\/oplus/" "${2}"
             sed -i "/<version>/d" "${2}"
             ;;
+            odm/etc/init/vendor.oplus.hardware.biometrics.fingerprint@2.1-service.rc)
+            sed -i "8i\    task_profiles ProcessCapacityHigh MaxPerformance" "${2}"
+            ;;
         odm/lib64/libCOppLceTonemapAPI.so|odm/lib64/libCS.so|odm/lib64/libSuperRaw.so|odm/lib64/libYTCommon.so|odm/lib64/libyuv2.so)
             "${PATCHELF_0_17_2}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
             ;;
